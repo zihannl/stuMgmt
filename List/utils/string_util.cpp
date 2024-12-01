9d6ace1220da1c18b09c11775cef432298720a89
zihannl <zihan2506@gmail.com>
2024-12-01 17:40:50 +0800

    Update string_util.cpp
    add trim_l function

--- List/utils/string_util.cpp
+++ List/utils/string_util.cpp
@@ -8,3 +8,11 @@ string_util::string_util()
 string_util::~string_util()
 {
 }
+
+std::string& string_util::trim_l(std::string &str)
+{
+	if (str.empty()) return str;
+
+	str.erase(0, str.find_first_not_of(" \n\r\t"));
+	return str;
+}
