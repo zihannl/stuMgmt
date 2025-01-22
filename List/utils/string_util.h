e8ba396e49431174a2db8060de68511b419d5aba
zihannl <zihan2506@gmail.com>
2025-01-23 04:58:31 +0800

    Update string_util.h

--- List/utils/string_util.h
+++ List/utils/string_util.h
@@ -41,5 +41,7 @@ public:
 	static std::string mid_l_r(std::string &str, std::string str_left, std::string str_right, bool b_inc_start = false, bool b_inc_end = false);
 	static std::string span_including(std::string& str, std::string str_key);
 	static std::string span_excluding(std::string& str, std::string str_key);
+
+	static bool start_with(std::string &str, std::string str_begin);
 };
 
